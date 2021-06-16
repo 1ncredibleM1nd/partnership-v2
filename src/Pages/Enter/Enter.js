3b6c197f539a9a7289e5ee1e382db34fec1d4f56
a.rodin <andierodin@gmail.com>
2021-06-16 17:55:41 +0300

    Add starting template for pages: Enter and Registration

---
+++ src/Pages/Enter/Enter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Enter =()=>{
+    return(
+        <div>
+
+        </div>
+    )
+};
+
+
+export default Enter;
---
+++ src/Pages/Reg/Reg.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+
+const Reg = ()=>{
+    return(
+        <div>
+
+        </div>
+
+
+    )
+}
+
+export default Reg;
